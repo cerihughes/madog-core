6078d1cd734da480d4960a477240abe04d3338b9
Ceri Hughes <github@cerihughes.co.uk>
2018-11-23 16:04:24 +0000

    Fixed typing issue

--- Sample/AppDelegate.swift
+++ Sample/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let window = UIWindow()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        guard let ui = NavigationUI<URL>(pageResolver: RuntimePageResolver()) else {
+        guard let ui = NavigationUI<ResourceLocator>(pageResolver: RuntimePageResolver()) else {
             return false
         }
 

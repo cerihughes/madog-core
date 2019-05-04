4a2e38f50c89c1c084622fa922686bcd123df79e
Ceri Hughes <github@cerihughes.co.uk>
2019-05-04 16:50:02 +0100

    Add some tests

--- Tests/MadogTypesTests.swift
+++ Tests/MadogTypesTests.swift
@@ -8,26 +8,90 @@
 
 import XCTest
 
+@testable import Madog
+
 class MadogTypesTests: XCTestCase {
 
+    private var registry: Registry<Int>!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+
+        registry = Registry()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        registry = nil
+
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMadogRegistry() {
+        let context = TestContext()
+        let uuid = registry.add(registryFunctionWithContext: createFunction(limit: 0))
+
+        XCTAssertNotNil(registry.createViewController(from: 0, context: context))
+        XCTAssertNil(registry.createViewController(from: 1, context: context))
+
+        registry.removeRegistryFunction(uuid: uuid)
+        XCTAssertNil(registry.createViewController(from: 0, context: context))
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testMadogResolver() {
+        // This mostly tests that the code compiles as expected... Don't need to exercise it much.
+
+        let resolver = TestResolver()
+        XCTAssertEqual(resolver.serviceProviderCreationFunctions().count, 1)
+        XCTAssertEqual(resolver.viewControllerProviderCreationFunctions().count, 1)
+    }
+}
+
+// MARK: - Test functions
+
+fileprivate func createFunction(limit: Int) -> (Int, Context) -> UIViewController? {
+    return { token, context in
+        guard token <= limit else {
+            return nil
         }
+
+        let vc = UIViewController()
+        vc.title = "function \(limit)"
+        return vc
+    }
+}
+
+private class TestViewControllerProvider: ViewControllerProvider<Int> {
+    private var uuid: UUID? = nil
+
+    override func register(with registry: Registry<Int>) {
+        uuid = registry.add(registryFunctionWithContext: createFunction(limit: 0))
     }
 
+    override func unregister(from registry: Registry<Int>) {
+        guard let uuid = uuid else {
+            return
+        }
+
+        registry.removeRegistryFunction(uuid: uuid)
+    }
+}
+
+private class TestServiceProvider: ServiceProvider {
+}
+
+private class TestResolver: Resolver<Int> {
+    override func viewControllerProviderCreationFunctions() -> [() -> ViewControllerProvider<Int>] {
+        let viewControllerProvider = { return TestViewControllerProvider() }
+        return [viewControllerProvider]
+    }
+
+    override func serviceProviderCreationFunctions() -> [(ServiceProviderCreationContext) -> ServiceProvider] {
+        let serviceProvider = { context in return TestServiceProvider(context: context) }
+        return [serviceProvider]
+    }
+}
+
+private class TestContext: Context {
+    func change<VC>(to identifier: SingleUIIdentifier<VC>, token: Any) -> Bool where VC : UIViewController { return false }
+    func change<VC>(to identifier: MultiUIIdentifier<VC>, tokens: [Any]) -> Bool where VC : UIViewController { return false }
 }

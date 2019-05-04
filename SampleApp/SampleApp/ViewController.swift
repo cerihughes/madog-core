a8df140c52d3adba8cc0465de28fc6111cac9ca5
Ceri Hughes <github@cerihughes.co.uk>
2019-05-04 16:55:26 +0100

    Create sample app skeleton

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Ceri Hughes on 04/05/2019.
+//  Copyright © 2019 Ceri Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

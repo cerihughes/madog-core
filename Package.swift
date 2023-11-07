2d920553b5542662e769558ab1e8b30a194d862a
cerihughes <github@cerihughes.co.uk>
2023-11-07 12:51:18 +0000

    Use released version of provident

--- Package.swift
+++ Package.swift
@@ -8,10 +8,16 @@ let package = Package(
         .library(name: "MadogCore", targets: ["MadogCore"])
     ],
     dependencies: [
-        .package(name: "Provident", url: "https://github.com/cerihughes/provident", .branch("align-with-madog")),
+        .package(url: "https://github.com/cerihughes/provident", .upToNextMajor(from: "6.0.0")),
     ],
     targets: [
-        .target(name: "MadogCore", dependencies: ["Provident"], path: "MadogCore"),
+        .target(
+            name: "MadogCore",
+            dependencies: [
+                .product(name: "Provident", package: "provident")
+            ],
+            path: "MadogCore"
+        ),
         .testTarget(name: "MadogCoreTests", dependencies: ["MadogCore"], path: "MadogCoreTests")
     ]
 )

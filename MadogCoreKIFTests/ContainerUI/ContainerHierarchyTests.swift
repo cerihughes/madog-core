55404a94e9a5751a5ba5242170d03cdd48b26647
cerihughes <github@cerihughes.co.uk>
2023-11-24 14:32:11 +0000

    Add failing test

---
+++ MadogCoreKIFTests/ContainerUI/ContainerHierarchyTests.swift
@@ -0,0 +1,30 @@
+//
+//  Created by Ceri Hughes on 20/11/2023.
+//  Copyright © 2023 Ceri Hughes. All rights reserved.
+//
+
+#if canImport(KIF) && canImport(UIKit)
+
+import KIF
+import MadogCoreTestUtilities
+import XCTest
+
+@testable import MadogCore
+@testable import MadogCoreTests
+
+class ContainerHierarchyTests: MadogKIFTestCase {
+    func testCloseReleasesMainContainer() {
+        let container = renderUIAndWait(identifier: .testTabBar(), tokenData: .multi(
+            .create(identifier: .testNavigation(), tokenData: .single("vc1")),
+            .create(identifier: .testNavigation(), tokenData: .single("vc2"))
+        ))
+        waitForLabel(token: "vc1")
+        waitForLabel(token: "vc2")
+        XCTAssertNotNil(container.castValue)
+
+        closeContainerAndWait(container)
+        XCTAssertNil(container.castValue)
+    }
+}
+
+#endif
